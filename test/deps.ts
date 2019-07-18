8d9955e48082b775ebb319773a4f994bd8982bc7
迷渡 <justjavac@gmail.com>
2019-07-18 08:20:01 +0800

    Bump CI to v0.12.0

--- test/deps.ts
+++ test/deps.ts
@@ -1,8 +1,8 @@
-export { test, runTests } from "https://deno.land/std@v0.5.0/testing/mod.ts";
+export { test, runTests } from "https://deno.land/std/testing/mod.ts";
 export {
   assert,
   assertEquals,
   assertNotEquals,
   assertThrowsAsync,
   assertThrows
-} from "https://deno.land/std@v0.5.0/testing/asserts.ts";
+} from "https://deno.land/std/testing/asserts.ts";
